1ffbef6d899e97018bd17df8de2da059a7a48e59
DCTL\mosheb <mosheb@webasgard.com>
2017-11-23 17:40:54 +0200

    first commit

---
+++ bootstrap/app.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start(); // set session
+
+use App\App;
+use Slim\Container;
+
+define('INC_ROOT', __DIR__);
+
+require INC_ROOT . '/../vendor/autoload.php'; // upload dependencies
+
+$app = new App(new Container(
+    include INC_ROOT . '/container.php'
+));
+
+$container = $app->getContainer();
+
+//$app = new Slim\App(); // slim instance
+
+/* test route 
+$app->get('/', function($request, $response) {
+   return 'Home';  
+});
+*/
+
+require INC_ROOT . '/../routes/web.php';
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+$app->get('/', function($request, $response) {
+    return 'Home';  
+});
+*/
+
+/*
+$app->get('/home', function($request, $response) {
+    return;
+});
+*/
+
+$app->get('/', function($request, $response) {
+    return 'Homepage';
+});
